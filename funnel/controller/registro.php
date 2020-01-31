b7c22419acba935b970bdaa89f014b48b538ff7c
Juank1530 <juank153021@gmail.com>
2020-01-31 17:55:19 -0500

    Fin css e inico registro.php 31/01/20

---
+++ funnel/controller/registro.php
@@ -0,0 +1,29 @@
+<?php
+
+if (isset($_GET['submit'])) {
+    $name = $_GET['value'];
+    echo $name;
+    echo 'H';
+}
+
+if (! $_GET) {
+    header('Location: http://localhost:90/Juank1530.github.io/funnel/view/index.html');
+}
+
+
+
+//
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
