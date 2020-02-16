fd9682a328e939d9461ae682c7c6a2fb3890a8c1
Juank1530 <Juank15021@gmail.com>
2020-02-16 02:26:05 -0500

    Cambio del archivo de conexión y algo de fechas

--- tests/date.php
+++ tests/date.php
@@ -8,6 +8,23 @@ echo '<pre>';
 echo print_r($time);
 echo '<pre/>';
 
+$week = [
+    "days" => 1,2,3,4,5,
+    "star" =>6,
+    "end" =>10
+];
 
+
+$weekend = [
+    "days" => 0,6,
+    "star" =>6,
+    "end" =>17
+];
+
+if ($week == $time) {
+    echo 'Entre semana';
+}elseif ($weekend == $time) {
+    echo 'fin de semana';
+}
 
 
