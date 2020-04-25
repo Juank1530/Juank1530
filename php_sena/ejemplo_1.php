171aa897634b47a37c7b07bb154c887000c608c4
Juank1530 <juank153021@gmail.com>
2020-04-25 12:53:20 -0500

    Datos demograficos y sena

---
+++ php_sena/ejemplo_1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html> <!-- En los archivos .php el código HTML se escribe normalmente-->
+    <head> <!-- como puedes ver toda la estructura de la página está escrita en HTML-->
+        <title>Ejemplo 1</title>
+        <meta http-equiv="Content-Type" 
+              content="text/html; charset=ISO-8859-1" />
+    </head>
+    <body>
+        <p> Esto es HTML puro</p> <!-- Esto está escrito en HTML-->
+        <?php
+            echo "Nombre: Juan Carlos Támara<br>"; /* Esto está escrito en PHP*/
+            echo "N° Cédula: 80921396<br>";
+            echo "Hola estimado EDWAR DE JESUS VALENCIA SANCHEZ, te saludo utilizando codigo PHP";
+        ?>
+    </body>
+</html>
