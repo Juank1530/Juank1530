21d0bb4f3cd5f3a57e73a5fdd3b722d5a34e2b66
JuanK_Office <Juank153021@gmail.com>
2020-04-25 13:44:54 -0500

    Platzi dibuju canvas

--- prog_basica_js_platzi/js/dibujo2.js
+++ prog_basica_js_platzi/js/dibujo2.js
@@ -1,74 +1,96 @@
+var texto = document.getElementById("textbox1");
+var boton = document.getElementById("button1");
+
+boton.addEventListener("click", DibujoPorClic);
+
+
 var d = document.getElementById("dibujo2");
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var xi, yi, xf, yf;
-var l = 0;
-var color_linea = "pink";
-var color_linea2 = "red";
-var color_linea3 = "green";
-var color_linea4 = "grey";
 
-dibujo_linea(color_linea, 0, 0, 0, 300);
-dibujo_linea2(color_linea, 1, 1, 1, 299);
-dibujo_linea2(color_linea, 1, 299, 299, 299);
 
-dibujo_linea(color_linea2, 0, 0, 300, 300);
-dibujo_linea2(color_linea2, 1, 1, 299, 1);
-dibujo_linea2(color_linea2, 299, 1, 299, 299);
 
-dibujo_linea(color_linea3, 300, 0, 0, 300);
-dibujo_linea2(color_linea3, 299, 1, 299, 299);
-dibujo_linea2(color_linea3, 299, 299, 1, 299);
 
-dibujo_linea(color_linea4, 0, 300, 300, 0);
-dibujo_linea2(color_linea4, 1, 1, 299, 1);
-dibujo_linea2(color_linea4, 1, 1, 1, 299);
 
-function dibujo_linea(color, xi, yi, xf, yf) {
-    if (xi == 300 && yi == 0 && xf == 0 && yf == 300 || xi == 0 && yi == 300 && xf == 300 && yf == 0) {
-        for (i = 0; i <= lineas; i++) {
-            xf = 300 - ((i + 1) * 10);
-            yi = 10 * i;
-            lienzo.beginPath();
-            lienzo.strokeStyle = color;
-            lienzo.moveTo(xi, yi);
-            lienzo.lineTo(xf, yf);
-            lienzo.stroke();
-            lienzo.closePath();
-            console.log("Línea" + i);
-        }
-    } else if (xi == 0 && yi == 0 && xf == 0 && yf == 300) {
-        for (i = 0; i <= lineas; i++) {
-            yi = 10 * i;
-            xf = 10 * (i + 1);
-            lienzo.beginPath();
-            lienzo.strokeStyle = color;
-            lienzo.moveTo(xi, yi);
-            lienzo.lineTo(xf, yf);
-            lienzo.stroke();
-            lienzo.closePath();
-            console.log("Línea" + i);
-        }
-    } else if (xi == 0 && yi == 0 && xf == 300 && yf == 300) {
-        for (i = 0; i <= lineas; i++) {
-            xi = 10 * i;
-            yf = 10 * (i + 1);
-            lienzo.beginPath();
-            lienzo.strokeStyle = color;
-            lienzo.moveTo(xi, yi);
-            lienzo.lineTo(xf, yf);
-            lienzo.stroke();
-            lienzo.closePath();
-            console.log("Línea" + i);
+
+
+
+function DibujoPorClic() {
+
+
+    var ancho_canvas = d.width;
+    var num_lineas = parseInt(texto.value);
+    var xi, yi, xf, yf;
+    var l = 0;
+    var color_linea = "pink";
+    var color_linea2 = "red";
+    var color_linea3 = "green";
+    var color_linea4 = "grey";
+    var espacio = ancho_canvas / num_lineas;
+
+    dibujo_linea(color_linea, 0, 0, 0, 300);
+    dibujo_linea2(color_linea, 1, 1, 1, 299);
+    dibujo_linea2(color_linea, 1, 299, 299, 299);
+
+    dibujo_linea(color_linea2, 0, 0, 300, 300);
+    dibujo_linea2(color_linea2, 1, 1, 299, 1);
+    dibujo_linea2(color_linea2, 299, 1, 299, 299);
+
+    dibujo_linea(color_linea3, 300, 0, 0, 300);
+    dibujo_linea2(color_linea3, 299, 1, 299, 299);
+    dibujo_linea2(color_linea3, 299, 299, 1, 299);
+
+    dibujo_linea(color_linea4, 0, 300, 300, 0);
+    dibujo_linea2(color_linea4, 1, 1, 299, 1);
+    dibujo_linea2(color_linea4, 1, 1, 1, 299);
+
+    function dibujo_linea(color, xi, yi, xf, yf) {
+        if (xi == 300 && yi == 0 && xf == 0 && yf == 300 || xi == 0 && yi == 300 && xf == 300 && yf == 0) {
+            for (i = 0; i <= num_lineas; i++) {
+                xf = 300 - ((i + 1) * espacio);
+                yi = espacio * i;
+                lienzo.beginPath();
+                lienzo.strokeStyle = color;
+                lienzo.moveTo(xi, yi);
+                lienzo.lineTo(xf, yf);
+                lienzo.stroke();
+                lienzo.closePath();
+                console.log("Línea" + i);
+            }
+        } else if (xi == 0 && yi == 0 && xf == 0 && yf == 300) {
+            for (i = 0; i <= num_lineas; i++) {
+                yi = espacio * i;
+                xf = espacio * (i + 1);
+                lienzo.beginPath();
+                lienzo.strokeStyle = color;
+                lienzo.moveTo(xi, yi);
+                lienzo.lineTo(xf, yf);
+                lienzo.stroke();
+                lienzo.closePath();
+                console.log("Línea" + i);
+            }
+        } else if (xi == 0 && yi == 0 && xf == 300 && yf == 300) {
+            for (i = 0; i <= num_lineas; i++) {
+                xi = espacio * i;
+                yf = espacio * (i + 1);
+                lienzo.beginPath();
+                lienzo.strokeStyle = color;
+                lienzo.moveTo(xi, yi);
+                lienzo.lineTo(xf, yf);
+                lienzo.stroke();
+                lienzo.closePath();
+                console.log("Línea" + i);
+            }
         }
     }
-}
 
-function dibujo_linea2(color, x_ini, y_ini, x_fin, y_fin) {
-    lienzo.beginPath();
-    lienzo.strokeStyle = color;
-    lienzo.moveTo(x_ini, y_ini);
-    lienzo.lineTo(x_fin, y_fin);
-    lienzo.stroke();
-    lienzo.closePath();
+    function dibujo_linea2(color, x_ini, y_ini, x_fin, y_fin) {
+        lienzo.beginPath();
+        lienzo.strokeStyle = color;
+        lienzo.moveTo(x_ini, y_ini);
+        lienzo.lineTo(x_fin, y_fin);
+        lienzo.stroke();
+        lienzo.closePath();
+    }
+
+
 }
